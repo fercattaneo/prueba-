e5700cad83b5d5e3cb738f73f49669cccacfc877
fercattaneo <fcattaneo@itba.edu.ar>
2021-10-19 10:22:44 -0300

    agregué esta parte del codigo hay que borrar la otra sin el .c

---
+++ tp9_2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int is_capicua(int* p1, int *p2, int cant);
+
+int main(void){
+	int numero[]={1,1};
+	int*p1=numero;
+	int*p2=&numero[1];
+	int result = is_capicua(p1, p2, 0);
+	printf("%d\n", result);
+	return 0;
+}
+
+int is_capicua(int* p1, int *p2, int cant){
+	cant--;
+	if (cant >0){
+		if(*(p1+cant)==*(p2-cant)){		
+			cant--;
+			return (is_capicua(p1, p2, (cant)));
+		}
+		else{
+			return 0;
+		}
+	}
+	else{
+		if(*p1==*p2){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+}
